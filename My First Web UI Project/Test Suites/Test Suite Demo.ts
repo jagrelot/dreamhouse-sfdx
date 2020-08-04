56efb49a079c3c58e1786e7b1bfa229e7b79df46
Jose Agrelot <jagrelot@acumensolutions.com>
2020-08-04 13:39:41 -0400

    Add Katalon

---
+++ My First Web UI Project/Test Suites/Test Suite Demo.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite Demo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fd40a3b3-caae-4cb8-b1e2-dde60a41756c</testSuiteGuid>
+   <testCaseLink>
+      <guid>06018c0a-5eec-4f4c-9a0a-01a3b1b6d923</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Test</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
